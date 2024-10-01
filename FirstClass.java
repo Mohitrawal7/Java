19175b9e75c1764eb10ef53df3cd94ee6064573f
Mohitrawal7 <rawalmohit12@gmail.com>
2024-10-01 17:00:53 +0545

    example  --java

--- FirstClass.java
+++ FirstClass.java
@@ -1,13 +1,29 @@
+import java.util.*;
 class FirstClass {
     public static void main(String args[]){
+        //output
       //  System.out.println("Hello World with java");
       // System.out.print("*\n**\n***");
+
       //variables
-      int a = 10;
+    /*   int a = 10;
       int b = 22;
       int sum = a+ b;
       System.out.println(sum);
       int diff= b-a;
       System.out.println(diff);
+      */
+
+      //input
+    //  Scanner sc = new Scanner(System.in);
+    //  String name = sc.next();
+    //  System.out.println(name);
+
+      //code
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int sum = a+b;
+      System.out.println(sum);
     }
 }
