f84ea71fcea3ef7b08d8ae740c7c0007436e6ba8
Mohitrawal7 <rawalmohit12@gmail.com>
2024-10-01 16:37:10 +0545

    output java-

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,6 @@
 class FirstClass {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.println("Hello World with java");
+        System.out.print("*\n**\n***");
     }
 }
