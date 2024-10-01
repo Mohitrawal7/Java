8c72bd41387e94b23370aea2336c34f76dc80354
Mohitrawal7 <rawalmohit12@gmail.com>
2024-10-01 16:44:36 +0545

    variable --java

--- FirstClass.java
+++ FirstClass.java
@@ -1,6 +1,13 @@
 class FirstClass {
     public static void main(String args[]){
-        System.out.println("Hello World with java");
-        System.out.print("*\n**\n***");
+      //  System.out.println("Hello World with java");
+      // System.out.print("*\n**\n***");
+      //variables
+      int a = 10;
+      int b = 22;
+      int sum = a+ b;
+      System.out.println(sum);
+      int diff= b-a;
+      System.out.println(diff);
     }
 }
