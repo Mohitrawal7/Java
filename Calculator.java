b04cc2351063c4cb65fe39f7a1481f2d30ba122b
Mohitrawal7 <rawalmohit12@gmail.com>
2024-10-04 20:35:22 +0545

    loop,pattern & calculator confused

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,16 @@
 import java.util.*;
 class Calculator {
     public static void main(String args[]){
+
+  char op;
  Scanner sc = new Scanner(System.in);
- System.out.println("Enter your two number accordingly \n");
+ System.out.println("Enter your two number accordingly");
  int a = sc.nextInt();
  int b = sc.nextInt();
+ double result;
+ result = sc.nextDouble();
  System.out.println("Enter the operation");
- char op = sc.nextChar();
+  op = input.next().charAt(0);
  switch(op){
  case "+" : int sum = a+b;
  System.out.println(sum);
@@ -15,13 +19,24 @@ class Calculator {
  int diff = a-b;
  System.out.println(diff);
  }else{
- diff = b-a;
+ int diff = b-a;
  System.out.println(diff);
  }
  break;
- case "*": int mul = a*b;
+ case "*":
+  int mul = a*b;
  System.out.println(mul);
  break;
+ case "/":
+ if (a>b){
+ int div =a/b;
+ }else{
+ int div = b/a;
+ }
+ System.out.println(div);
+ break;
+
   }
+  input.close();
  }
 }
