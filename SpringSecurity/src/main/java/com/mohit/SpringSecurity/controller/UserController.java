a50ba43cd32ede278ba730794da28a377816685d
Mohitrawal7 <rawalmohit12@gmail.com>
2025-01-28 04:56:08 +0545

    jwt implement verify and generate

--- SpringSecurity/src/main/java/com/mohit/SpringSecurity/controller/UserController.java
+++ SpringSecurity/src/main/java/com/mohit/SpringSecurity/controller/UserController.java
@@ -19,4 +19,12 @@ public class UserController {
         return service.register(user);
     }
 
+    @PostMapping("/login")
+    public String login(@RequestBody Users user){
+
+
+
+        return service.verify(user);
+    }
+
 }
