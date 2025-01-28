279e0678a8840b20f8a1f829d184048a6ee0010e
Mohitrawal7 <rawalmohit12@gmail.com>
2025-01-28 06:02:06 +0545

    oauth implement on github and google

---
+++ SpringoAuth2/src/main/java/com/mohit/SpringoAuth2/SpringoAuth2Application.java
@@ -0,0 +1,13 @@
+package com.mohit.SpringoAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringoAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringoAuth2Application.class, args);
+	}
+
+}
---
+++ SpringoAuth2/src/test/java/com/mohit/SpringoAuth2/SpringoAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.SpringoAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
